817425a338484b700803550c8ff85a87e80ae649
Gabe G. <gabegullion@icloud.com>
2020-03-05 12:28:48 -0600

    Added classes to make the pong example runnable. It can now be modified to work for our game.

--- src/main/java/cmsc250/mazerunnerclient/GameGateway.java
+++ src/main/java/cmsc250/mazerunnerclient/GameGateway.java
@@ -1,9 +1,101 @@
 package cmsc250.mazerunnerclient;
 
-public class GameGateway implements Constants{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Platform;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
-    void close() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+/**
+ *
+ * @author Joe Gregg
+ */
+public class GameGateway implements Constants {
+    private PrintWriter outputToServer;
+    private BufferedReader inputFromServer;
+    private List<Shape> shapes;
+    Rectangle leftPaddle;
+    Rectangle rightPaddle;
+    Circle ball;
+    boolean isOpen = true;
+    
+    public GameGateway() {
+        try {
+            // Create a socket to connect to the server
+            Socket socket = new Socket("localhost", 8000);
+
+            // Create an output stream to send data to the server
+            outputToServer = new PrintWriter(socket.getOutputStream());
+
+            // Create an input stream to read data from the server
+            inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        } catch (IOException ex) {
+            System.out.println("Exception in GameGateway.");
+            ex.printStackTrace();
+        }
+        
+        // Make the shapes
+        shapes = new ArrayList<Shape>();
+        
+        leftPaddle = new Rectangle(MARGIN,MARGIN,THICKNESS,LENGTH);
+        leftPaddle.setFill(Color.BLUE);
+        shapes.add(leftPaddle);
+        
+        rightPaddle = new Rectangle(WIDTH-MARGIN-THICKNESS,MARGIN,THICKNESS,LENGTH);
+        rightPaddle.setFill(Color.BLUE);
+        shapes.add(rightPaddle);
+        
+        ball = new Circle(WIDTH/2,HEIGHT/4,MARGIN/4);
+        ball.setFill(Color.RED);
+        shapes.add(ball);
     }
     
+    public List<Shape> getShapes() { return shapes; }
+    
+    // Move the player's paddle
+    public synchronized void movePaddle(boolean up) {
+        if(up)
+            outputToServer.println(MOVE_UP);
+        else
+            outputToServer.println(MOVE_DOWN);
+        outputToServer.flush();
+    }
+
+    // Refresh the game state
+    public synchronized void refresh() {
+        outputToServer.println(GET_GAME_STATE);
+        outputToServer.flush();
+        String state = "";
+        try {
+            state = inputFromServer.readLine();
+        } catch (IOException ex) {
+            System.out.println("Exception in GameGateway.");
+            ex.printStackTrace();
+        }
+        String parts[] = state.split(" ");
+        ball.setCenterX(Double.parseDouble(parts[0]));
+        ball.setCenterY(Double.parseDouble(parts[1]));
+        leftPaddle.setY(Double.parseDouble(parts[2]));
+        rightPaddle.setY(Double.parseDouble(parts[3]));
+    }
+    
+    public void close() {
+        try {
+        outputToServer.close();
+        inputFromServer.close();
+        } catch(Exception ex) {
+            
+        }
+        isOpen = false;
+    }
+    
+    public boolean open() { return isOpen; }
 }
--- src/main/java/cmsc250/mazerunnerclient/GamePane.java
+++ src/main/java/cmsc250/mazerunnerclient/GamePane.java
@@ -1,13 +1,62 @@
 package cmsc250.mazerunnerclient;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
+/**
+ *
+ * @author Joe Gregg
+ */
 public class GamePane extends Pane {
-
-    GamePane(GameGateway gateway) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private GameGateway gateway;
+    
+    public GamePane(GameGateway gateway) {
+        this.gateway = gateway;
+        this.getChildren().addAll(gateway.getShapes());
+        this.setOnKeyPressed(e->handleKey(e));
+        this.setMinWidth(USE_PREF_SIZE);
+        this.setMaxWidth(USE_PREF_SIZE);
+        this.setPrefWidth(Constants.WIDTH);
+        this.setMinHeight(USE_PREF_SIZE);
+        this.setMaxHeight(USE_PREF_SIZE);
+        this.setPrefHeight(Constants.HEIGHT);
+        new Thread(new UpdateGameState(gateway)).start();
+    }
+    
+    private void handleKey(KeyEvent evt) {
+        KeyCode code = evt.getCode();
+        if(code == KeyCode.UP)
+            gateway.movePaddle(true);
+        else if(code == KeyCode.DOWN)
+            gateway.movePaddle(false);
     }
 
-  
+    @Override
+    public boolean isResizable() {
+        return false; 
+    }
     
+}
+
+class UpdateGameState implements Runnable {
+    private GameGateway gateway;
+    
+    public UpdateGameState(GameGateway gateway) {
+        this.gateway = gateway;
+    }
+    
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep((long) 0000.1);
+                if(gateway.open())
+                    gateway.refresh();
+                else
+                    break;
+            } catch(Exception ex) {
+                
+            }
+        }
+    }
 }
