bd9f333505c2650b1d0b347eaa35e0980093223e
Gabe G. <gabegullion@icloud.com>
2020-03-08 19:25:59 -0500

    Added player life labels (which don't get updated yet) and started working on the player win method.

--- src/main/java/cmsc250/mazerunnerclient/GameGateway.java
+++ src/main/java/cmsc250/mazerunnerclient/GameGateway.java
@@ -12,22 +12,30 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 /**
  *
  * @author Joe Gregg
  */
 public class GameGateway implements Constants {
+
     private PrintWriter outputToServer;
     private BufferedReader inputFromServer;
     private List<Shape> shapes;
+    private GamePane pane;
     Rectangle leftPaddle;
     Rectangle rightPaddle;
     Rectangle leftStart;
     Rectangle rightStart;
+    Rectangle win1;
+    Rectangle win2;
+    Label lives1;
+    Label lives2;
     Circle ball;
     boolean isOpen = true;
-    
+
     public GameGateway() {
         try {
             // Create a socket to connect to the server
@@ -43,43 +51,47 @@ public class GameGateway implements Constants {
             System.out.println("Exception in GameGateway.");
             ex.printStackTrace();
         }
-        
+
         // Make the shapes
         shapes = new ArrayList<Shape>();
-        
+
         // Player 1
-        leftPaddle = new Rectangle(MARGIN,MARGIN,THICKNESS,LENGTH);
+        leftPaddle = new Rectangle(MARGIN, MARGIN, THICKNESS, LENGTH);
         leftPaddle.setFill(Color.web("#cc3300")); //RED
         leftPaddle.setStroke(Color.BLACK);
         shapes.add(leftPaddle);
-        
+
         // Player 2
-        rightPaddle = new Rectangle(WIDTH-MARGIN-THICKNESS,MARGIN,THICKNESS,LENGTH);
+        rightPaddle = new Rectangle(WIDTH - MARGIN - THICKNESS, MARGIN, THICKNESS, LENGTH);
         rightPaddle.setFill(Color.web("#3366cc")); //BLUE
         rightPaddle.setStroke(Color.BLACK);
         shapes.add(rightPaddle);
 
         // Ball
-        ball = new Circle(WIDTH/2,HEIGHT/4,MARGIN/4);
+        ball = new Circle(WIDTH / 2, HEIGHT / 4, MARGIN / 4);
         ball.setFill(Color.BLACK);
         shapes.add(ball);
+
     }
-    
-    public List<Shape> getShapes() { return shapes; }
-    
+
+    public List<Shape> getShapes() {
+        return shapes;
+    }
+
     /* Move the player's paddle
     *  1 -> increases x or y
     *  -1 -> decreases x or y
-    */
+     */
     public synchronized void movePaddle(int y, int x) {
-        if(y == 1)
+        if (y == 1) {
             outputToServer.println(MOVE_UP);
-        else if(y == -1)
+        } else if (y == -1) {
             outputToServer.println(MOVE_DOWN);
-        else if(x == 1)
+        } else if (x == 1) {
             outputToServer.println(MOVE_LEFT);
-        else if(x == -1)
+        } else if (x == -1) {
             outputToServer.println(MOVE_RIGHT);
+        }
         outputToServer.flush();
     }
 
@@ -98,23 +110,62 @@ public class GameGateway implements Constants {
         String parts[] = state.split(" ");
         ball.setCenterX(Double.parseDouble(parts[0]));
         ball.setCenterY(Double.parseDouble(parts[1]));
-        leftPaddle.setY(Double.parseDouble(parts[2]));
-        rightPaddle.setY(Double.parseDouble(parts[3]));
+
+        // Check if player 1(RED) has reached goal
+        if (Double.parseDouble(parts[2]) > HEIGHT - 25) {
+            // Give player win
+            win(1);
+        } else {
+            leftPaddle.setY(Double.parseDouble(parts[2]));
+        }
+
+        // Check if player 2(BLUE) has reached goal
+        if (Double.parseDouble(parts[3]) > HEIGHT - 25) {
+            // Give player win
+            win(2);
+        } else {
+            rightPaddle.setY(Double.parseDouble(parts[3]));
+        }
+
         // Added these lines for client to get the x coords from server
         leftPaddle.setX(Double.parseDouble(parts[4]));
         rightPaddle.setX(Double.parseDouble(parts[5]));
 
     }
-    
+
+    // Winner screen
+    public void win(int player) {
+        if (player == 1) {
+            win1 = new Rectangle();
+            win1.setFill(Color.web("#cc3300")); //RED
+            win1.setX(0);
+            win1.setY(0);
+            win1.setWidth(WIDTH);
+            win1.setHeight(HEIGHT);
+            pane.getChildren().add(win1);
+        } else {
+            win2 = new Rectangle();
+            win2.setFill(Color.web("#3366cc")); //BLUE
+            win2.setX(0);
+            win2.setY(0);
+            win2.setWidth(WIDTH);
+            win2.setHeight(HEIGHT);
+            pane.getChildren().add(win2);
+
+        }
+    }
+
     public void close() {
         try {
-        outputToServer.close();
-        inputFromServer.close();
-        } catch(Exception ex) {
-            
+            outputToServer.close();
+            inputFromServer.close();
+        } catch (Exception ex) {
+
         }
         isOpen = false;
     }
-    
-    public boolean open() { return isOpen; }
+
+    public boolean open() {
+        return isOpen;
+    }
 }
--- src/main/java/cmsc250/mazerunnerclient/GamePane.java
+++ src/main/java/cmsc250/mazerunnerclient/GamePane.java
@@ -4,6 +4,7 @@ import static cmsc250.mazerunnerclient.Constants.HEIGHT;
 import static cmsc250.mazerunnerclient.Constants.WIDTH;
 import static cmsc250.mazerunnerclient.Constants.MARGIN;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
@@ -12,16 +13,23 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+
 /**
  *
  * @author Joe Gregg
  */
-public class GamePane extends Pane  {
+public class GamePane extends Pane {
 
     private GameGateway gateway;
+    private Pane pane;
     private Rectangle goal;
     private Rectangle start1;
     private Rectangle start2;
+    private Rectangle win1;
+    private Rectangle win2;
+    private Label lives1;
+    private Label lives2;
 
     public GamePane(GameGateway gateway) {
         // Player 1 start area
@@ -30,20 +38,18 @@ public class GamePane extends Pane  {
         start1.setStroke(Color.BLACK);
         start1.setX(0);
         start1.setY(0);
-        start1.setWidth(MARGIN*3);
-        start1.setHeight(MARGIN*3);
-        this.getChildren().add(start1);
+        start1.setWidth(MARGIN * 3);
+        start1.setHeight(MARGIN * 3);
 
         // Player 2 start area
-        start1 = new Rectangle();
-        start1.setFill(Color.web("#339933")); //GREEN
-        start1.setStroke(Color.BLACK);
-        start1.setX(580.0);
-        start1.setY(0);
-        start1.setWidth(MARGIN*3);
-        start1.setHeight(MARGIN*3);
-        this.getChildren().add(start1);
-        
+        start2 = new Rectangle();
+        start2.setFill(Color.web("#339933")); //GREEN
+        start2.setStroke(Color.BLACK);
+        start2.setX(580.0);
+        start2.setY(0);
+        start2.setWidth(MARGIN * 3);
+        start2.setHeight(MARGIN * 3);
+
         // Goal area rectangle
         goal = new Rectangle();
         goal.setFill(Color.web("#339933")); //GREEN
@@ -52,8 +58,28 @@ public class GamePane extends Pane  {
         goal.setY(HEIGHT - 50.0);
         goal.setWidth(WIDTH);
         goal.setHeight(HEIGHT);
-        this.getChildren().add(goal);
-        
+
+        // Player 1 life label
+        lives1 = new Label("Lives: 5");
+        lives1.setFont(new Font("Helvetica Bold", 16));
+        lives1.setTextFill(Color.WHITE);
+        lives1.setMinWidth(20);
+        lives1.setMinHeight(10);
+        lives1.setLayoutX(0);
+        lives1.setLayoutY(0);
+
+        // Player 2 life label
+        lives2 = new Label("Lives: 5");
+        lives2.setFont(new Font("Helvetica Bold", 16));
+        lives2.setTextFill(Color.WHITE);
+        lives2.setMinWidth(20);
+        lives2.setMinHeight(10);
+        lives2.setLayoutX(580.0);
+        lives2.setLayoutY(0);
+
+        // Add all to pane
+        this.getChildren().addAll(start1, start2, goal, lives1, lives2);
+
         this.gateway = gateway;
         this.getChildren().addAll(gateway.getShapes());
         this.setOnKeyPressed(e -> handleKey(e));
@@ -63,8 +89,8 @@ public class GamePane extends Pane  {
         this.setMinHeight(USE_PREF_SIZE);
         this.setMaxHeight(USE_PREF_SIZE);
         this.setPrefHeight(Constants.HEIGHT);
-        this.setBackground(new Background(new BackgroundFill(Color.web("#424242"), CornerRadii.EMPTY, Insets.EMPTY)));
-        
+        this.setBackground(new Background(new BackgroundFill(Color.web("#424242"), CornerRadii.EMPTY, Insets.EMPTY))); //DARK GREY
+
         new Thread(new UpdateGameState(gateway)).start();
     }
 
